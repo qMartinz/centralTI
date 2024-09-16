6c9925338e18c74bd0e4d7a05b2bd795dd6a7f39
Guga <86026460+qMartinz@users.noreply.github.com>
2024-09-16 13:09:47 -0300

    aaaa

---
+++ script/inicio.js
@@ -0,0 +1,35 @@
+let user = "Guga"
+
+function onLogin(){
+    var content = document.getElementById('content');
+    content.innerHTML = `<iframe src="html/inicio.html" onload="this.insertAdjacentHTML('afterend', (this.contentDocument.body||this.contentDocument).innerHTML);this.remove();welcome();"></iframe>`;
+}
+
+function onLoginError(err){
+    console.error('Error on login', err);
+}
+
+function onLogout(){
+    var content = document.getElementById('content');
+    content.innerHTML = '';
+}
+
+function welcome(){
+    gapi.client.load('people', 'v1', function() {
+        gapi.client.people.people.get({
+            resourceName: "people/me",
+            personFields: "names"
+        }).then((response) => {
+            document.getElementById('user').textContent = `Olá, ${response.result.names[0].givenName}!`;
+        }, (error) => {
+            console.error('Error on info get', error);
+        });
+    });
+
+    document.getElementById('date').textContent = `Hoje é ${new Date().toLocaleDateString('pt-BR', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })}!`;
+}
